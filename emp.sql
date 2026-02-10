a498525472f26f2eea53fcba1a290bb64f5d28f9
Poorva1810 <harshida849@gmail.com>
2026-02-10 22:14:25 +0530

    done

---
+++ emp.sql
@@ -0,0 +1,14 @@
+create table if not EXISTS empl(
+Eno char(3),
+Ename varchar(15),
+age int(2),
+gen char(1),
+sal int(7),
+exp int(2),
+loc varchar(15),
+dno char(3)
+);
+
+insert into empl VALUES
+('E
+
